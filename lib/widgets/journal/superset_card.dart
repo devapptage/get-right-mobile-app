642283fd6e13834beebcd5e3dbeac957b4e638ff
shujaat khan <shujaatkhan5000@gmail.com>
2026-02-12 22:46:18 +0500

    Updated code push

--- lib/widgets/journal/superset_card.dart
+++ lib/widgets/journal/superset_card.dart
@@ -27,7 +27,6 @@ class SupersetCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Superset Header
           // Container(
           //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           //   decoration: BoxDecoration(
